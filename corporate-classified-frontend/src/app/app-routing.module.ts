c994dd5af6532b9823ced450e7f35af56e0427b1
Kiran GK <Kiran.Gopalakrishnan@ust.com>
2022-11-16 15:37:23 +0530

    components and path set

--- corporate-classified-frontend/src/app/app-routing.module.ts
+++ corporate-classified-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MyOffersComponent } from './my-offers/my-offers.component';
+import { OfferDetailComponent } from './offer-detail/offer-detail.component';
+import { PostOfferComponent } from './post-offer/post-offer.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'profile', component: ProfileComponent},
+  {path: 'post-offer', component: PostOfferComponent},
+  {path: 'offer-detail', component: OfferDetailComponent},
+  {path: 'my-offers', component: MyOffersComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'contact', component:ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

46855c56d8b18b0de85262278d5dae70648ad211
Prince joy <233693@ust-global.com>
2022-11-28 09:33:10 +0530

    changes made

--- corporate-classified-frontend/src/app/my-offers/my-offers.component.ts
+++ corporate-classified-frontend/src/app/my-offers/my-offers.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyOffersComponent implements OnInit {
 
+  pageTitle: string = "My Offers";
+  post: string = "19/11/2022";
+  engage: string = "not yet";
+  status: string = "open";
+  category: string = "Apartments";
+  price: string = "20K";
+  Negotiable: string = "Yes";
+  Likes: number = 3;
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }

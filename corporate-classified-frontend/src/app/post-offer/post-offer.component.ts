9a6ccd5f03797c680bbf59e285ac851d029cb080
Kiran GK <Kiran.Gopalakrishnan@ust.com>
2022-11-28 18:09:18 +0530

    no changes

--- corporate-classified-frontend/src/app/post-offer/post-offer.component.ts
+++ corporate-classified-frontend/src/app/post-offer/post-offer.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Route, Router } from '@angular/router';
+//import { Offer } from '../class/offer';
+import { Category } from '../interfaces/category';
+import { Offer } from '../interfaces/offer';
+import { OfferService } from '../services/offer/offer.service';
 
 @Component({
   selector: 'app-post-offer',
@@ -6,10 +12,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-offer.component.css']
 })
 export class PostOfferComponent implements OnInit {
-
-  constructor() { }
+  offerForm: FormGroup | any;
+  category: Category[] = [];
+  // offer: Offer = new Offer("","",0,0);
+  //offerForm: FormGroup;
+  //offerForm: Offer | undefined;
+  constructor(private offerService: OfferService, private formBuilder: FormBuilder, private route:Router) { }
 
   ngOnInit(): void {
+
+    this.offerService.getCategory().subscribe(
+      data => {
+        this.category = data;
+      }
+    );
+
+    this.offerForm = this.formBuilder.group({
+      offerTitle: new FormControl('', Validators.minLength(2)),
+      offerDescription: new FormControl('', Validators.maxLength(100)),
+      category: new FormControl(),
+      offerPrice: new FormControl(0),
+      offerNegotiable: new FormControl(false)
+    });
+
+    Object.assign(
+      {},this.offerForm.value,{
+        category:{
+          categoryId: this.offerForm.get('category')?.value},
+      }
+    );
   }
 
+ 
+
+  onSubmit(){
+    //this.offer.offerDescription = this.offerForm.value.offerDescription
+   this.offerService.save(Object.assign(
+    {},this.offerForm.value,{
+      category:{
+        categoryId: this.offerForm.get('category')?.value},
+    }
+  )).subscribe(data=>{
+    this.route.navigate(['/main-page/employee-home']);
+   });
+   console.log(JSON.stringify(this.offerForm.value))
+  }
 }
--- corporate-classified-frontend/src/app/services/offer/offer.service.ts
+++ corporate-classified-frontend/src/app/services/offer/offer.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from 'src/app/interfaces/category';
+import { Offer } from 'src/app/interfaces/offer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OfferService {
 
-  constructor() { }
+  springUrl: string = "http://localhost:8080/";
+
+  constructor(private http:HttpClient) { }
+
+  save(offer: Offer){
+    const headers = new HttpHeaders({'Content-Type': 'application/json; charset=utf-8'})
+    console.log(offer);
+   // const httpHeader = new Headers()
+    return this.http.post<Offer>(this.springUrl+"post-offer",offer,{headers,observe:'response',responseType:'json'});
+  }
+
+  getCategory(): Observable<Category[]>{
+    return this.http.get<Category[]>(this.springUrl+"category")
+  }
 }
